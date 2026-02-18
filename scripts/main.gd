f7c4edb1db55f613b200fa2698c3f742b7fb6594
Aashraya Belbase <sparkdark103@gmail.com>
2026-02-18 18:43:31 +0545

    Added Enemy Mechanisms

---
+++ scripts/main.gd
@@ -0,0 +1,47 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+
+
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+
+func _on_mob_timer_timeout() -> void:
+	#Create new instance of mob in scene
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = $MobPath/MobSpawnLocation  #choosing random location in path
+	mob_spawn_location.progress_ratio = randf()
+	mob.position = mob_spawn_location.position   #set mob position in random location
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	#randomness in direction
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	#choose velocity for mob
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	#spawn mob by adding it to main scene
+	add_child(mob)
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
